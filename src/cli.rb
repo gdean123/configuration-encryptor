e00e3eae16661dbb89fafe693a83aec10f5f142b
George Dean <gdean123@gmail.com>
2017-01-08 17:17:57 -0800

    Create encryptor show --environment <environment> that shows all key/value pairs for a given environment

--- src/cli.rb
+++ src/cli.rb
@@ -18,24 +18,48 @@ class Cli < Thor
   option :value, required: true
   option :environment, required: true
   def encrypt
-    public_key = Keypair.read_public_key(options[:environment])
-    encrypted_value = Encryptor.encrypt(options[:value], public_key)
-    EncryptedValue.write(options[:environment], options[:key], encrypted_value)
+    encrypt_value(options[:key], options[:value], options[:environment])
   end
 
   desc 'decrypt', 'Decrypt a value using the private key'
   option :key, required: true
   option :environment, required: true
   def decrypt
-    private_key = Keypair.read_private_key(options[:environment])
-    encrypted_value = EncryptedValue.read(options[:environment], options[:key])
-    puts Encryptor.decrypt(encrypted_value, private_key)
+    puts decrypt_value(options[:key], options[:environment])
   end
 
   desc 'keys', 'Show all keys for an environment'
   option :environment, required: true
   def keys
-    keys_path = EncryptionPaths.environment(options[:environment])
-    puts Dir.entries("#{keys_path}").select {|file| !File.directory? file}
+    puts keys_for_environment(options[:environment])
+  end
+
+  desc 'show', 'Show all key/value pairs for an environment'
+  option :environment, required: true
+  def show
+    keys = keys_for_environment(options[:environment])
+    keys.each do |key|
+      value = decrypt_value(key, options[:environment])
+      puts "#{key} = #{value}"
+    end
+  end
+
+  private
+
+  def encrypt_value(key, value, environment)
+    public_key = Keypair.read_public_key(options[:environment])
+    encrypted_value = Encryptor.encrypt(options[:value], public_key)
+    EncryptedValue.write(options[:environment], options[:key], encrypted_value)
+  end
+
+  def decrypt_value(key, environment)
+    private_key = Keypair.read_private_key(environment)
+    encrypted_value = EncryptedValue.read(environment, key)
+    Encryptor.decrypt(encrypted_value, private_key)
+  end
+
+  def keys_for_environment(environment)
+    keys_path = EncryptionPaths.environment(environment)
+    Dir.entries("#{keys_path}").select {|file| !File.directory? file}
   end
 end
